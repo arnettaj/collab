ee9ba9d031855e4725b0bf1540fc1b49d4a624af
bauerbp <bauerbp@mail.uc.edu>
2015-05-19 18:15:43 -0400

    pretty much done

--- ChoiArnettBauer1.cpp
+++ ChoiArnettBauer1.cpp
@@ -1,4 +1,4 @@
-//Author: Aaron Choi Austin Arnett Brian Bauer
+//Author: Aaron Choi, Austin Arnett, Brian Bauer
 //Course title: Data Structures
 //Course Number: CS2028
 #include <iostream>
@@ -13,21 +13,21 @@ using namespace std;
 class Polygon{
 
   public:
-    virtual int area() = 0;
-    virtual int perimeter() = 0;
-    
-    
+    virtual int area(){return 0;}
+    virtual int perimeter(){return 0;}
+
+
 
 };
 //there are also types of triangles hence the use of more inheritance
 class Triangle: public Polygon{
-  
+
   public:
 
     virtual int area(){
-      return 0;  
+      return 0;
     }
-    
+
     virtual int perimeter(){
       return 0;
     }
@@ -35,24 +35,35 @@ class Triangle: public Polygon{
 };
 class Isosceles: public Triangle{
 //todo take x and y find hypotenuse
-//todo find area 
-//todo add 3 sides together to find perimeter  
+//todo find area
+//todo add 3 sides together to find perimeter
   public:
     int area(int x, int y){
-      
-      return 0;
+      float hypo;
+      float answer;
+      if(x>y){
+        hypo= sqrt(pow(x,2)-pow(y,2));
+        answer=(y/2)*hypo;
+      }
+      else if(y>x){
+        hypo= sqrt(pow(y,2)-pow(x,2));
+        answer=(x/2)*hypo;
+      }
+      return hypo;
     }
     int perimeter(int x, int y){
-      
-      return 0;
+      int answer;
+      if(x>=y){answer=2*x+y;}
+      else if(y>=x){answer=2*y+x;}
+      return answer;
     }
 
 };
 class Equallateral: public Triangle{
   //todo find area using side basic geometry remember the angles and stuff im too lazy
   public:
-    int area(int side){
-      return 1;
+    float area(int side){
+      return (pow(3, .5)/4)*pow(side, 2);
     }
     int perimeter(int side){
       return side*3;
@@ -60,7 +71,7 @@ class Equallateral: public Triangle{
 
 };
 class Quadrilateral: public Polygon{
-  
+
   public:
     virtual int area(){
       return 0;
@@ -80,7 +91,7 @@ class Square: public Quadrilateral{
     int perimeter(int side){
       return side * 4;
     }
-    
+
 
 };
 class Rectangle: public Quadrilateral{
@@ -97,10 +108,10 @@ class Rectangle: public Quadrilateral{
 class Pentagon: public Polygon{
 //theres a weird formula for area implement it
   public:
-    int area(int side){
-      return 0;
+    float area(int side){
+      return (1.0/4.0)*sqrt(5*(5+2*sqrt(5)))*pow(side, 2);
     }
-    
+
     int perimeter(int side){
       return side * 5;
     }
@@ -109,8 +120,8 @@ class Pentagon: public Polygon{
 class Hexagon: public Polygon{
 //implement area of hexagon
   public:
-    int area(int side){
-      return 0;
+    float area(int side){
+      return (3/2)*sqrt(3)*pow(side,2);
     }
     int perimeter(int side){
       return side * 6;
@@ -120,8 +131,8 @@ class Hexagon: public Polygon{
 class Octagon: public Polygon{
 //implement area of octogon
   public:
-    int area(int side){
-      return 0;
+    float area(int side){
+      return 2*(1+sqrt(2))*pow(side, 2);
     }
 
     int perimeter(int side){
@@ -132,10 +143,10 @@ class Octagon: public Polygon{
 int main() {
   string polygon_type = "";
   string secondary_type = "";
-  
+
   cout << "Is your polygon a triangle, quadrilateral, pentagon, hexagon or octagon?\n";
   cin >> polygon_type;
-  
+
   if(polygon_type == "triangle"){
     cout << "Is your triangle a isolsceles or equalateral triangle?";
     cin >> secondary_type;
@@ -148,14 +159,14 @@ int main() {
       Equallateral triangle;
       cin >> side;
       cout << triangle.area(side) << "is your area and your perimeter is: " << triangle.perimeter(side) << "\n";
-      
+
     }
   }
-  
+
   else if(polygon_type == "quadrilateral"){
     cout << "Is your quadrilateral a square or a rectangle? ";
     cin >> secondary_type;
-    
+
     if(secondary_type == "square"){
       cout << "Please enter the length of a side of the square ";
       int side = 0;
@@ -163,7 +174,7 @@ int main() {
       Square s;
       cout << s.area(side) << " is your area " << s.perimeter(side) << " is your perimeter" << "\n";
     }
-    
+
     else if(secondary_type == "rectangle"){
       cout << "Please enter the height: ";
       int height = 0;
@@ -174,24 +185,34 @@ int main() {
       Rectangle r;
       cout << r.area(height, width) << " is your area " << r.perimeter(height, width) << " is your perimeter" << "\n";
     }
-    
+
   }
   else if(polygon_type == "pentagon"){
-    cout << "Please enter the length of one of the sides.";
-  
+    cout << "Please enter the length of the side.";
+      int side = 0;
+      cin >> side;
+      Pentagon p;
+      cout << p.area(side) << " is your area " << p.perimeter(side) << " is your perimeter." << "\n";
   }
   else if(polygon_type == "hexagon"){
-  
     cout << "Please enter the length of one of the sides.";
+    int side;
+    cin>>side;
+    Hexagon h;
+    cout<<h.area(side) <<" is the area "<<h.perimeter(side)<<" is the perimeter. \n";
   }
   else if(polygon_type == "octagon"){
-  
+
     cout << "Please enter the length of one of the sides.";
+    int side;
+    cin>>side;
+    Octagon o;
+    cout<<o.area(side)<<" is the area and "<<o.perimeter(side)<<" is the perimeter. \n";
   }
   else{
-  
+
     cout << "Something went wrong. Please try again.";
-  } 
-  
+  }
+
   return 0;
 }
